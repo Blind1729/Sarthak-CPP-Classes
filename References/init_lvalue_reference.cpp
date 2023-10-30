5034f6cb11ea47fe87aec9c708c28d6b4759fc8f
Arun Suryan <2018PGCACA47@nitjsr.ac.in>
2023-10-30 19:26:56 +0530

    Add references code

---
+++ References/init_lvalue_reference.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    int &invalidRef; // error: references must be initialized
+    int x{5};
+    int &ref{x}; // okay: reference to int is bound to int variable
+    return 0;
+}
---
+++ References/lvalue-reference.cpp
@@ -0,0 +1,3 @@
+int          // a normal int type
+    int &    // an lvalue reference to an int object
+    double & // an lvalue reference to a double object
